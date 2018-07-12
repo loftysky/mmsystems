2b9ad39a86eee6e49fff2653f6c501198cffeb18
Mike Boers <mikeb@loftysky.com>
2018-07-12 15:51:47 -0400

    mminstall-arnold --farmsoup

--- mmsystems/install/apps/arnold.py
+++ mmsystems/install/apps/arnold.py
@@ -10,6 +10,7 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-V', '--maya-version', default='2018')
+    parser.add_argument('-F', '--farmsoup', action='store_true')
     parser.add_argument('--no-sudo', action='store_true')
     args = parser.parse_args()
 
@@ -28,12 +29,16 @@ def main():
             'silent', # Accept the EULA, etc..
         ])
 
-    for resource in ('mtoa', 'mtoa{}'.format(args.maya_version)):
-        farmsoup_line = '''WORKER_RESOURCES['{}'] = 'Inf' # mminstall-arnold'''.format(resource)
-        farmsoup_path = '/etc/farmsoup.py'
-        farmsoup_done = os.path.exists(farmsoup_path) and farmsoup_line in open(farmsoup_path).read()
-        if not farmsoup_done:
-            subprocess.check_call(['bash', '-c', 'echo "{}" >> {}'.format(farmsoup_line, farmsoup_path)])
+    if args.farmsoup:
+        for resource in (
+            'arnold',
+            'mtoa{}'.format(args.maya_version),
+        ):
+            farmsoup_line = '''WORKER_RESOURCES['{}'] = 'Inf' # mminstall-arnold'''.format(resource)
+            farmsoup_path = '/etc/farmsoup.py'
+            farmsoup_done = os.path.exists(farmsoup_path) and farmsoup_line in open(farmsoup_path).read()
+            if not farmsoup_done:
+                subprocess.check_call(['bash', '-c', 'echo "{}" >> {}'.format(farmsoup_line, farmsoup_path)])
 
 
 if __name__ == '__main__':
